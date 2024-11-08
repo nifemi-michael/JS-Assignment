9ff66c393866fa2272bd79a80cf0b92015fade4a
Nifemi Michael <nifemi.ire2022@gmail.com>
2024-11-08 21:21:50 +0100

    Assignment

---
+++ script.js
@@ -0,0 +1,43 @@
+// ASSIGNMENT //
+
+
+
+// DOUBLE A NUMBER //
+
+// function doubleNum(num) {
+//     return num * 2;
+// }
+// console.log(doubleNum(8)) //
+
+
+
+
+
+// AREA OF A RECTANGLE //
+
+// function calculateArea(length, breadth) {
+//     return length * breadth;
+// }
+
+// console.log(calculateArea(5, 67))
+
+
+
+
+
+// POSITIVITY OF A NUMBER //
+
+// function positive(num) {
+//     return num > 0;
+// }
+// console.log(positive(-2))
+
+
+
+
+// CUBE OF A NUMBER
+
+function cube(num) {
+    return num ** 3;
+}
+console.log(cube(4))
