24363c661dbbc4db10357247c2d771dba2965447
Nifemi Michael <nifemi.ire2022@gmail.com>
2024-11-15 21:38:20 +0100

    update

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
-// ASSIGNMENT //
+// // ASSIGNMENT //
 
 
 
-// DOUBLE A NUMBER //
+// // DOUBLE A NUMBER //
 
-console.log("DOUBLE A NUMBER ")
+// console.log("DOUBLE A NUMBER ")
 
-function doubleNum(num) {
-    return num * 2;
-}
-console.log(doubleNum(8));
-
-
-
-
-
-// AREA OF A RECTANGLE //
-
-console.log("AREA OF A RECTANGLE")
-
-function calculateArea(length, breadth) {
-    return length * breadth;
-}
-
-console.log(calculateArea(5, 67));
+// function doubleNum(num) {
+//     return num * 2;
+// }
+// console.log(doubleNum(8));
 
 
 
 
 
-// POSITIVITY OF A NUMBER //
+// // AREA OF A RECTANGLE //
 
-console.log("POSITIVITY OF A NUMBER")
+// console.log("AREA OF A RECTANGLE")
 
-function positive(num) {
-    return num > 0;
-}
-console.log(positive(-2));
+// function calculateArea(length, breadth) {
+//     return length * breadth;
+// }
+
+// console.log(calculateArea(5, 67));
 
 
 
 
-// CUBE OF A NUMBER
 
-console.log("CUBE OF A NUMBER")
+// // POSITIVITY OF A NUMBER //
 
-function cube(num) {
-    return num ** 3;
-}
-console.log(cube(4))
+// console.log("POSITIVITY OF A NUMBER")
+
+// function positive(num) {
+//     return num > 0;
+// }
+// console.log(positive(-2));
+
+
+
+
+// // CUBE OF A NUMBER
+
+// console.log("CUBE OF A NUMBER")
+
+// function cube(num) {
+//     return num ** 3;
+// }
+// console.log(cube(4))
+
+
+
+//  FIRST CAPITAL LETTER
+let userName = prompt('Enter your name');
+let firstSlice = userName.slice(0,1)
+let secondSlice = userName.slice(1)
+let userFirstCap = firstSlice.toUpperCase()
+let merge = userFirstCap.concat(secondSlice)
+console.log(merge)
